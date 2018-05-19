6c9db037bf8361b7ac91ab19c0e35ee617e2ae39
Jodom <jodomouma@gmail.com>
2018-05-19 19:23:13 +0300

    add register and login pages

--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -1,4 +1,6 @@
 import { LandingPage } from "./landingPage";
 import { ProfilePage } from "./profilePage";
+import { LoginPage } from "./loginPage";
+import { RegisterPage } from "./registerPage";
 
-export {LandingPage, ProfilePage}
+export { LandingPage, ProfilePage, LoginPage, RegisterPage }
