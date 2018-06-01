9e4cb262cbd077e282c844cb6ec5b97f861b8c4f
Jodom <jodomouma@gmail.com>
2018-06-01 13:43:22 +0300

    hybrid reviews slider

--- src/components/Landing/modules/Reviews.js
+++ src/components/Landing/modules/Reviews.js
@@ -1,7 +1,44 @@
 import React from 'react';
 
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+
+// import actions
+import { getReviews } from "../../../actions/reviewsActions";
+
+import { Row, Col } from "reactstrap";
+
 class Reviews extends React.Component {
+
+    componentDidMount() {
+        this.props.getReviews();
+    }
+
     render () {
+        const reviews = this.props.reviews.map( review => {
+            return (
+                <div key={review.id} className="w-slide">
+                    <div className="reviews-wrapper">
+                        <Row className="justify-content-center">
+                            <Col md="5" className="column-3 w-col">
+                                <div className="rev-photo-wrapper">
+                                    <div className="rev-photo" style={{ backgroundImage: `url(${review.profile.photo})` }}></div>
+                                </div>
+                            </Col>
+                            <Col md="7">
+                                <div className="rev-content">
+                                    <div className="sub-second-text rev">{review.review}</div>
+                                    <div className="top-margin less">
+                                        <h4 className="heading">{review.profile.name}</h4>
+                                        <p className="sub-rev-text">{review.profile.role}</p>
+                                    </div>
+                                </div>
+                            </Col>
+                        </Row>
+                    </div>
+                </div>
+            )
+        })
         return (
             <div className="section" id="reviews">
                 <div className="container w-container">
@@ -14,52 +51,9 @@ class Reviews extends React.Component {
                         </div>
                     </div>
                     <div>
-                        <div data-animation="outin" data-duration="500" data-infinite="1" className="testi-slider w-slider">
+                        <div data-animation="outin" data-duration="500" data-infinite="1" className="review-slider">
                             <div className="w-slider-mask">
-                                <div className="w-slide">
-                                    <div className="testimonials-wrapper">
-                                        <div className="flex-row w-row">
-                                            <div className="column-3 w-col w-col-5">
-                                                <div className="test-wrapper-photo">
-                                                    <div className="testi-photo"></div>
-                                                    <div className="testi-overlay-1"></div>
-                                                    <div className="testi-overlay-1 second"></div>
-                                                </div>
-                                            </div>
-                                            <div className="w-col w-col-7">
-                                                <div className="testi-content">
-                                                    <div className="sub-second-text testi">Suspendisse nisl elit, rhoncus eget, elementum ac, condimentum eget, diam. Fusce fermentum odio nec</div>
-                                                    <div className="top-margin less">
-                                                        <h4 className="heading">Natasha Joe</h4>
-                                                        <p className="sub-testi-text">CO Founder of Design Studio</p>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="w-slide">
-                                    <div className="testimonials-wrapper">
-                                        <div className="flex-row w-row">
-                                            <div className="column-3 w-col w-col-5">
-                                                <div className="test-wrapper-photo">
-                                                    <div className="testi-photo _2"></div>
-                                                    <div className="testi-overlay-1"></div>
-                                                    <div className="testi-overlay-1 second"></div>
-                                                </div>
-                                            </div>
-                                            <div className="w-col w-col-7">
-                                                <div className="testi-content">
-                                                    <div className="sub-second-text testi">Suspendisse nisl elit, rhoncus eget, elementum ac, condimentum eget, diam. Fusce fermentum odio nec</div>
-                                                    <div className="top-margin less">
-                                                        <h4 className="heading">Michael Lee</h4>
-                                                        <p className="sub-testi-text">Designer at Webflow</p>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
+                                { reviews }
                             </div>
                             <div className="right-arrow w-slider-arrow-left">
                                 <img src="assets/img/arrow-left.svg" width="200" />
@@ -76,4 +70,16 @@ class Reviews extends React.Component {
     }
 }
 
-export default Reviews;
+function mapStateToProps(state) {
+    return {
+        reviews: state.reviews.reviews
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({
+        getReviews: getReviews
+    }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Reviews);
--- src/reducers/reviewReducers.js
+++ src/reducers/reviewReducers.js
@@ -1,6 +1,27 @@
 export function reviewReducers(
     state = {
-        reviews: []
+        reviews: [
+            {
+                id: 1,
+                profile: {
+                    name: "Beth Yang",
+                    photo: "http://uploads.webflow.com/5a7d94ae2dbf1b0001dc27df/5a7ec4ebee248c0001380b3e_team-5.jpg",
+                    role: "Electrical Engineering Student, JKUAT"
+                },
+                review: "With Spark, I am able to set learning milestones, and watch myself achieve them. Whats more, I get feedback on what worked and what failed along the journey",
+                rating: 5
+            },
+            {
+                id: 2,
+                profile: {
+                    name: "Charlie Lee",
+                    photo: "http://uploads.webflow.com/5a7d94ae2dbf1b0001dc27df/5a7f05f8c7942700013316bc_team-7.jpg",
+                    role: "Developer, Microsoft"
+                },
+                review: "Spark has continually guided me in my learning, leaving me to focus on the learning and not how I am learning",
+                rating: 5
+            }
+        ]
     }, action ){
         switch(action.type) {
             case 'GET_REVIEWS':
